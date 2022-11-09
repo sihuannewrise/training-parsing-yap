22246dbf11a2df6cadd6801280d83905367903b4
Сергей Кузин <kuzin-sergey@yandex.ru>
2022-11-09 11:06:13 +0300

    Ya.practicum parsers

---
+++ latest_versions.py
@@ -0,0 +1,35 @@
+import re
+import requests_cache
+from bs4 import BeautifulSoup
+
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+
+if __name__ == '__main__':
+    session = requests_cache.CachedSession()
+    response = session.get(MAIN_DOC_URL)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, features='lxml')
+
+    sidebar = soup.find('div', attrs={'class': 'sphinxsidebarwrapper'})
+    ul_tags = sidebar.find_all('ul')
+
+    for ul in ul_tags:
+        if 'All versions' in ul.text:
+            a_tags = ul.find_all('a')
+            break
+    else:
+        raise Exception('Ничего не нашлось')
+
+    results = []
+    pattern = r'Python (?P<version>\d\.\d+) \((?P<status>.*)\)'
+    for a_tag in a_tags:
+        link = a_tag['href']
+        text_match = re.search(pattern, a_tag.text)
+        if text_match is None:
+            version, status = a_tag.text, ''
+        else:
+            version, status = text_match.groups()
+        results.append((link, version, status))
+
+    for row in results:
+        print(*row)
---
+++ soup2.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+
+price_html = """
+<table cellspacing="0" cellpadding="0" border="1">
+  <tbody>
+    <tr class="even_row">
+      <th><p>№ п/п</p></th>
+      <th class="armor"><p>Название</p></th>
+      <th class="price"><p>Цена</p><p>рублей</p></th>
+    </tr>
+    <tr class="odd_row">
+      <td><p>1.</p></td>
+      <td class="armor"><p>Щит</p></td>
+      <td class="price"><p>375</p></td>
+    </tr>
+    <tr class="even_row">
+      <td><p>2.</p></td>
+      <td class="armor"><p>Шлем</p></td>
+      <td class="price"><p>297</p></td>
+    </tr>
+    <tr class="odd_row">
+      <td><p>3.</p></td>
+      <td class="armor"><p>Кольчуга</p></td>
+      <td class="price"><p>565</p></td>
+    </tr>
+    <tr class="even_row">
+      <td><p>4.</p></td>
+      <td class="armor"><p>Булава</p></td>
+      <td class="price"><p>1992</p></td>
+    </tr>
+    <!-- Сюда может добавиться неизвестное количество элементов экипировки.
+      Их тоже нужно учитывать при расчёте средней цены. -->
+  </tbody>
+</table>
+"""
+
+soup = BeautifulSoup(price_html, features='lxml')
+rows = soup.find_all('tr')
+num = len(rows) - 1
+total = 0
+for row in rows:
+    item = row.find('td', class_='price')
+    if item is not None:
+        total += int(item.find('p').text)
+result = total / num
+print('Средняя цена богатырских доспехов: ', result, 'рублей')

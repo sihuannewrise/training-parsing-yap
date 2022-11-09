8d0606fe1aa0164d82a956bc12ecfb3d801952d6
sihuannewrise <sihuannewrise@gmail.com>
2022-11-09 16:45:07 +0300

    murzik

---
+++ 26_arg_parsing.py
@@ -0,0 +1,30 @@
+import argparse
+
+MURZIK = '=^..^=______/'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Вежливый скрипт')
+    parser.add_argument('name', help='Имя')
+    parser.add_argument('-s', '--surname', help='Фамилия')
+    parser.add_argument(
+        '-c',
+        '--city',
+        help='Город',
+        choices=['Chekhov', 'Dublin', 'Minsk', 'Simbirsk'],
+    )
+    parser.add_argument(
+        '-m',
+        '--murzik',
+        action='store_true',
+        help=f'Отправить кота Мурзика {MURZIK}'
+    )
+    args = parser.parse_args()
+    parts = []
+    parts.append(f'Hello, {args.name}')
+    if args.surname is not None:
+        parts.append(args.surname)
+    if args.city is not None:
+        parts.append(f'from {args.city}')
+    if args.murzik:
+        parts.append(MURZIK)
+    print(*parts)

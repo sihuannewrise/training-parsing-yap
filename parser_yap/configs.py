fb899cacd5a7346c06d19ad17189bee3b166f253
sihuannewrise <sihuannewrise@gmail.com>
2022-11-10 12:09:02 +0300

    parser-yap folder

---
+++ parser_yap/configs.py
@@ -0,0 +1,23 @@
+import argparse
+
+
+def configure_argument_parser(available_modes):
+    parser = argparse.ArgumentParser(description='Парсер документации Python')
+    parser.add_argument(
+        'mode',
+        choices=available_modes,
+        help='Режимы работы парсера',
+    )
+    parser.add_argument(
+        '-c',
+        '--clear-cache',
+        action='store_true',
+        help='Очистка кеша'
+    )
+    parser.add_argument(
+        '-p',
+        '--pretty',
+        action='store_true',
+        help='Вывод в формате PrettyTable'
+    )
+    return parser
---
+++ parser_yap/main.py
@@ -0,0 +1,114 @@
+import re
+import requests_cache
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from tqdm import tqdm
+
+from configs import configure_argument_parser
+from constants import BASE_DIR, MAIN_DOC_URL
+from outputs import control_output
+
+
+def whats_new(session):
+    whats_new_url = urljoin(MAIN_DOC_URL, 'whatsnew/')
+    response = session.get(whats_new_url)
+    response.encoding = 'utf-8'
+
+    soup = BeautifulSoup(response.text, features='lxml')
+
+    main_div = soup.find('section', attrs={'id': 'what-s-new-in-python'})
+    div_with_ul = main_div.find('div', attrs={'class': 'toctree-wrapper'})
+    sections_by_python = div_with_ul.find_all(
+        'li', attrs={'class': 'toctree-l1'})
+
+    results = [('Ссылка на статью', 'Заголовок', 'Редактор, автор')]
+
+    for section in tqdm(sections_by_python):
+        version_a_tag = section.find('a')
+        href = version_a_tag['href']
+        version_link = urljoin(whats_new_url, href)
+        response = session.get(version_link)
+        response.encoding = 'utf-8'
+        soup = BeautifulSoup(response.text, features='lxml')
+        h1 = soup.find('h1')
+        dl = soup.find('dl')
+        dl_text = dl.text.replace('\n', ' ')
+        results.append((version_link, h1.text, dl_text))
+
+    return results
+
+
+def latest_versions(session):
+    response = session.get(MAIN_DOC_URL)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, features='lxml')
+
+    sidebar = soup.find('div', attrs={'class': 'sphinxsidebarwrapper'})
+    ul_tags = sidebar.find_all('ul')
+
+    for ul in ul_tags:
+        if 'All versions' in ul.text:
+            a_tags = ul.find_all('a')
+            break
+    else:
+        raise Exception('Ничего не нашлось')
+
+    results = [('Ссылка на документацию', 'Версия', 'Статус')]
+
+    pattern = r'Python (?P<version>\d\.\d+) \((?P<status>.*)\)'
+    for a_tag in a_tags:
+        link = a_tag['href']
+        text_match = re.search(pattern, a_tag.text)
+        if text_match is None:
+            version, status = a_tag.text, ''
+        else:
+            version, status = text_match.groups()
+        results.append((link, version, status))
+
+    return results
+
+
+def download(session):
+    downloads_url = urljoin(MAIN_DOC_URL, 'download.html')
+    response = session.get(downloads_url)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, features='lxml')
+
+    pattern = r'.?pdf-a4\.zip$'
+
+    table_tag = soup.find('table', attrs={'class': 'docutils'})
+    pdf_a4_tag = table_tag.find('a', {'href': re.compile(pattern)})
+    pdf_a4_link = pdf_a4_tag['href']
+
+    archive_url = urljoin(downloads_url, pdf_a4_link)
+    filename = archive_url.split('/')[-1]
+    downloads_dir = BASE_DIR / 'downloads'
+    downloads_dir.mkdir(exist_ok=True)
+    archive_path = downloads_dir / filename
+
+    response = session.get(archive_url)
+    with open(archive_path, 'wb') as file:
+        file.write(response.content)
+
+
+MODE_TO_FUNCTION = {
+    'whats-new': whats_new,
+    'latest-versions': latest_versions,
+    'download': download,
+}
+
+
+def main():
+    arg_parser = configure_argument_parser(MODE_TO_FUNCTION)
+    args = arg_parser.parse_args()
+    session = requests_cache.CachedSession()
+    if args.clear_cache:
+        session.cache.clear()
+    parser_mode = args.mode
+    results = MODE_TO_FUNCTION[parser_mode](session)
+    if results is not None:
+        control_output(results, args)
+
+
+if __name__ == '__main__':
+    main()

f1b67710d3a502e2ba9dc56ee7710584033a48f4
sihuannewrise <sihuannewrise@gmail.com>
2022-11-21 18:00:43 +0300

    sqlalchemy

--- 34_work_with_db.py
+++ 34_work_with_db.py
@@ -1,4 +1,34 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import Session, declared_attr, declarative_base
+
+
+class Base:
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    id = Column(Integer, primary_key=True)
+
+
+Base = declarative_base(cls=Base)
+
+
+class Pep(Base):
+    pep_number = Column(Integer, unique=True)
+    name = Column(String(200))
+    status = Column(String(20))
+
+    def __repr__(self):
+        return f'PEP {self.pep_number} {self.name}'
+
 
 if __name__ == '__main__':
-    engine = create_engine('sqlite:///sqlite.db', echo=True)
+    engine = create_engine('sqlite:///sqlite.db', echo=False)
+    session = Session(engine)
+
+    results = session.query(Pep).filter(Pep.status == 'Active')
+    # Переменная results хранит объект Query...
+    print(type(results))
+    # ...который содержит только те объекты модели Pep, у которых поле status == 'Active'
+    print(results.all())
